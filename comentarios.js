0aded5f00adfa33d424162c69cef2f64436332b0
Gabriel Gomes <116276311+Gabriel-Yuki@users.noreply.github.com>
2025-02-01 02:33:02 -0300

    spotify react
    fazendo esse aplicativo

---
+++ comentarios.js
@@ -0,0 +1,15 @@
+/* Comandos - usar no pronpt comando ou no terminal
+
+npx create-react-app spotify-react
+or
+npx create-react-app@latest spotify-react
+
+cd spotify-react/
+
+npm install @testing-library/react@latest web-vitals
+
+code .
+
+npm start
+
+*/
